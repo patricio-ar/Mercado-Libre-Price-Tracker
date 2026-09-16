e3661ab910375769913a2533c7cc4d83e356fac4
patricio-ar <spatricio.ar@gmail.com>
2026-09-16 11:50:57 -0300

    Add menu loop

---
+++ main.py
@@ -0,0 +1,28 @@
+while True:
+
+    #Menu
+
+    print("=== Price Tracker ===")
+    print("1. Add a product")
+    print("2. Update a price")
+    print("3. Show my products")
+    print("4. Exit")
+
+    #Choose an option
+
+    option = input("Choose an option: ")
+
+    #If elif else
+
+    if option == "1":
+        print("Coming soon...")
+    elif option == "2":
+        print("Coming soon...")
+    elif option == "3":
+        print("Coming soon...")
+    elif option == "4":
+        print("Goodbye!")
+        break 
+    else:
+        print("Invalid option.")
+    
