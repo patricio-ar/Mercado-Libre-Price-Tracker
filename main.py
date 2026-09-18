2d651c032ff578cf853b64da009ac768db929795
patricio-ar <spatricio.ar@gmail.com>
2026-09-18 11:37:30 -0300

    Update prices and compare

--- main.py
+++ main.py
@@ -22,14 +22,38 @@ while True:
         tracker.add_product(products, name, price, target_price )
         storage.save_products(products)
         print("Product added!")
+
     elif option == "2":
-        print("Coming soon...")
+        for number, product in enumerate(products, start=1):
+            print(f"{number}. {product["name"]} - ${tracker.current_price(product)}")
+
+        choice = int(input("Product number: "))
+        product = products[choice-1]
+
+        new_price = int(input("New price: "))
+        difference = tracker.update_price(product, new_price)
+
+        storage.save_products(products)
+        if difference < 0:
+            print(f"The price went down ${abs(difference)}")
+        elif difference > 0:
+            print(f"The price went up ${difference}.")
+        else:
+            print("The price did not change.")
+
+        if new_price <= product["target_price"]:
+            print("It's below your target price")
+        
     elif option == "3":
         if len(products) == 0:
             print("You don't have any products yet.")
         else: 
             for product in products:
-                print(f"Product name: {product["name"]} - Current price: ${tracker.current_price(product)}")
+                print(f"Product name: {product["name"]} - Current price: ${tracker.current_price(product)} - Target: ${product["target_price"]}")
+                for entry in product["history"]:
+                    print(f"{entry["date"]}: ${entry["price"]}")
+                
+
     elif option == "4":
         print("Goodbye!")
         break
--- tracker.py
+++ tracker.py
@@ -13,3 +13,7 @@ def add_product(products, name, price, target_price):
 def current_price(product):
     return product["history"][-1]["price"]
 
+def update_price(product, new_price):
+    old_price =  current_price(product)
+    product["history"].append({"date": date.today().isoformat(), "price": new_price})
+    return new_price - old_price
